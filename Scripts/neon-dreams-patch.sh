7dfb9dee5d9d10a869ffb26c0afdd1ac335f7d3a
Paul Biberstein <biberstein.paul@gmail.com>
2021-07-14 21:36:25 -0400

    add new snippets and scripts

---
+++ Scripts/neon-dreams-patch.sh
@@ -0,0 +1,2 @@
+chmod a+w /opt/visual-studio-code/resources/app/out/vs/code/electron-browser/workbench/workbench.html
+chmod a+w /opt/visual-studio-code/resources/app/out/vs/code/electron-browser/workbench/neondreams.js
